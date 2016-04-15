257559f62832942a62a6e5070bc1bbacc4e3cfec
camscars <camscars702@gmail.com>
2016-04-15 09:02:54 -0700

    requirements.txt

--- landing/landing/celery.py
+++ landing/landing/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 #set default django settings for 'celery program'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landing.settings')
 
-app = Celery('landng')
+app = Celery('landing')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
