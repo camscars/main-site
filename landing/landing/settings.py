8a4ff3afbee0a02dfa6564e8a19f64665be0f7fa
camscars <camscars702@gmail.com>
2016-05-03 16:45:53 -0700

    Store HTML
    bringing the store html up to date with the rest of the site. Work in
    progress...

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&87#2=81i3mt2kb-b970sdz!=m#3cv@^4+o27#xlm77&p8%jyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
