84a3d46a6d58d583fd6372471cec3572f07018de
camscars <camscars702@gmail.com>
2016-05-03 17:36:31 -0700

    aws settings

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -90,12 +90,28 @@ WSGI_APPLICATION = 'landing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mydatabasename',
+            'USER': 'databaseuser',
+            'PASSWORD': 'secretpassword',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 HAYSTACK_CONNECTIONS = {
 'default': {
