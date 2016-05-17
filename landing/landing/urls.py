cd494554ac749ed27b0f9f7e4aca61502f886c27
camscars <camscars702@gmail.com>
2016-05-16 17:37:07 -0700

    updated with new projects
    Added new projects to main page and cleaned up the UI in a few places

--- landing/landing/urls.py
+++ landing/landing/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from newspage.views import LinkListView
 
 #Update the depreciated ones to include versions soon and
 #remember to make sure shop url works
@@ -29,6 +30,7 @@ urlpatterns = [
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
     url(r'^orders/', include('orders.urls', namespace='orders')),
+    url(r'^news/', LinkListView.as_view(), name='newspage'),
 ]
 
 if settings.DEBUG:
